d20dae6e493840309de042c5a53190a253366128
alrayyes <alrayyes@gmail.com>
2016-07-19 10:45:24 +0200

    Update wp-config.php

--- wordpress/files/wp-config.php
+++ wordpress/files/wp-config.php
@@ -80,6 +80,16 @@ define('WPLANG', '');
  */
 define('WP_DEBUG', false);
 
+/**
+ * Enable multisite
+ */
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', '{{ url }}');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
